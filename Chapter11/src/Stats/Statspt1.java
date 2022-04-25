6cc85ec790c0238dce98547981d0179d723bc6f9
904354510 <904354510@L815WD220034.edu.cbe.ab.ca>
2022-04-25 11:56:05 -0600

    aptil25

--- Chapter11/src/Stats/Statspt1.java
+++ Chapter11/src/Stats/Statspt1.java
@@ -7,6 +7,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Random;
 
 /*
 Program: Statspt1      Date: April 13, 2022
@@ -21,35 +24,38 @@ Course: Computer Science 30
 
 public class Statspt1 {
 
-
-
-	
-	
-	public static void main(String[] args) {
+	public static String main(String[] args) {
 
 		File textFile = new File("C:\\Users\\904354510\\git\\CS30M\\Chapter11\\src\\Stats\\test1.dat");
 		FileReader in;
 		BufferedReader readFile;
 		String name;
-		String score;
+		String score = null;
 		double avg;
 		double totalscore = 0;
-		int numscore = 0;		
+		int numscore = 0;
+		ArrayList nounList = new ArrayList(); 
 
 		try {
 			in = new FileReader(textFile);
 			readFile = new BufferedReader(in);
 			while ((name = readFile.readLine()) != null) {
 				score = readFile.readLine();
-		 	numscore +=1;
-		 	totalscore += Double.parseDouble(score);
-			System.out.println(name);	
-		 	System.out.println(score);
+				numscore += 1;
+				totalscore += Double.parseDouble(score);
+				System.out.println(name);
+				System.out.println(score);
 			}
 			avg = totalscore / numscore;
-			System.out.println("the average is:  " + avg);	
+			System.out.println("the average is:  " + avg);
 
-			readFile.close();
+			if (Integer.parseInt(score)>Integer.parseInt(score))
+			{
+				System.out.println(score);
+			}
+			
+			nounList.add(score);
+			
 			in.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("File does not exist or could not be found.");
@@ -58,7 +64,9 @@ public class Statspt1 {
 			System.out.println("Problem reading file.");
 			System.err.println("IOException: " + e.getMessage());
 		}
-
+		{return((String)nounList.get()));}
+		
+		
 	}
 }
 
