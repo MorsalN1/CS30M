d8ee57065c26bfa66c4cc19b85b3f65985faf0c1
904354510 <904354510@L815WD220034.edu.cbe.ab.ca>
2022-05-12 09:54:34 -0600

    may12

---
+++ Chapter9/src/CRT.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class CRT {
+/*
+ * 1. index of 2
+ * 2. quantities = new int [20]
+ * 3. int[] heights = {1.65,2.15,4.95};
+ * 4.for (Int element : grades) {
+ 			System.out.println(element);
+			}
+ * 5. 
+ * 	a)ArrayList<String> name = new ArrayList<String>();
+	
+	name.add(2, "roxy");
+	 for (String names : name) {
+		 System.out.println(name);
+	 }
+	b)ArrayList<String> name = new ArrayList<String>();
+	
+	 name.add("roxy");
+	 name.add("sam");
+	 name.add("tim");
+	 name.remove(2)
+	 for (String names : name) {
+		 System.out.println(name);
+	 }
+
+ * 6. An entire array is passed as a parameter and is a reference data type.
+ * A single element passes the data stored instead of a reference.
+ * 
+ * 7. Because when the range is to be shifted to correlate to a lower range of index values
+ * 
+ * 8. i
+ * (returns a char value that correspond to the letter at position index)
+ * 
+ * 
+ * 
+
+ */
+}
